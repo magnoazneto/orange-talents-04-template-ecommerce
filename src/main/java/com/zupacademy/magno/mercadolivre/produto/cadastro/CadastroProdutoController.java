a55a96c4bbce658d9f1422019e8fbaa6f708bf84
magnoazneto <magnoazneto@gmail.com>
2021-04-27 10:56:49 -0300

    [feat] altera objeto de response no controller de produtos para o requisitado

--- src/main/java/com/zupacademy/magno/mercadolivre/produto/cadastro/CadastroProdutoController.java
+++ src/main/java/com/zupacademy/magno/mercadolivre/produto/cadastro/CadastroProdutoController.java
@@ -3,6 +3,7 @@ package com.zupacademy.magno.mercadolivre.produto.cadastro;
 import com.zupacademy.magno.mercadolivre.produto.Produto;
 import com.zupacademy.magno.mercadolivre.produto.ProdutoRequest;
 import com.zupacademy.magno.mercadolivre.usuario.Usuario;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/produto")
@@ -24,9 +24,9 @@ public class CadastroProdutoController {
 
     @PostMapping
     @Transactional
-    public String criaProduto(@RequestBody @Valid ProdutoRequest request, @AuthenticationPrincipal Usuario usuario){
+    public ResponseEntity<?> criaProduto(@RequestBody @Valid ProdutoRequest request, @AuthenticationPrincipal Usuario usuario){
         Produto novoProduto = request.toModel(manager, usuario);
         manager.persist(novoProduto);
-        return request.toString();
+        return ResponseEntity.ok().build();
     }
 }
